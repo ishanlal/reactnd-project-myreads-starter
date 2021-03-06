b93f9a1387e01fb1437aa59da1516e8a95312eef
Ishan Lal <ishanlal@gmail.com>
2021-03-05 16:53:42 -0800

    Adding Search Capability

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
+import Search from './Search'
 
 class BooksApp extends React.Component {
   state = {
@@ -10,68 +11,30 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-     books: [],
-     s_books: [],
-     query: '',
-    showSearchPage: false
+     all_books: [],
+     showSearchPage: false
   }
 
-  updateQuery = (query) => {
-  this.setState(() => ({
-    query: query,
-  }));
-  BooksAPI.search(query.trim()).then((results) => {
-    this.setState(() => ({ s_books: results }));
-  });
-};
+  toggleVar = () => {
+    this.setState(() => ({
+      showSearchPage: false
+    }));
+  };
 
-  clearQuery = () =>{
-    this.updateQuery('')
-  }
-
-componentDidMount() {
+  componentDidMount() {
     BooksAPI.getAll()
     .then((books)=> {
       this.setState(() => ({
-        books
+        all_books: books
       }))
     })
   }
-
-
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text"
-                       placeholder="Search by title or author"
-                       value={this.state.query}
-                       onChange={(e) => this.updateQuery(e.target.value)}
-                       />
-                {
-                          this.state.query !== '' &&
-                          (<div className="displaySearchResults">
-                            <p>{this.state.query}</p>
-        				            </div>)}
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
+          <Search onToggleVar={this.toggleVar} />
+          ) : (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
---
+++ src/Search.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+import * as BooksAPI from './BooksAPI';
+import './App.css';
+
+class Search extends Component {
+  state = {
+    query: '',
+    searched_books: []
+  }
+
+updateQuery = (query) => {
+  this.setState( () => ({query: query}) );
+  BooksAPI.search(query.trim()).then((results) => {  this.setState(() => ({ searched_books: results }));  });
+};
+
+clearQuery = () => {
+  this.updateQuery('');
+};
+
+handleBookSelect = (book, shelf_name) => {
+    BooksAPI.update(book, shelf_name);
+    console.log('updated');
+};
+
+render() {
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <button className="close-search" onClick={() => this.props.onToggleVar()}>Close</button>
+        <div className="search-books-input-wrapper">
+          {/*
+            NOTES: The search from BooksAPI is limited to a particular set of search terms.
+            You can find these search terms here:
+            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+            you don't find a specific author or title. Every search is limited by search terms.
+          */}
+          <input type="text"
+                 placeholder="Search by title or author"
+                 value={this.state.query}
+                 onChange={(e) => this.updateQuery(e.target.value)}
+                 />
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid">
+        {
+          this.state.searched_books.map((book) => (
+            <li key={book.id} className='book-list-item'>
+            <div className="book">
+              <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`
+              }}>
+              </div>
+                <div className="book-shelf-changer">
+                  <select>
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading" onSelect={ () => {this.handleBookSelect(book, "currentlyReading")} }>Currently Reading</option>
+                    <option value="wantToRead" onSelect={ () => {this.handleBookSelect(book, "wantToRead")} }>Want to Read</option>
+                    <option value="read" onSelect={ () => {this.handleBookSelect(book, "read")} }>Read</option>
+                    <option value="none" onSelect={ () => {this.handleBookSelect(book, "none")} }>None</option>
+                  </select>
+                </div>
+              </div>
+              <div className="book-title">{book.title}</div>
+              <div className="book-authors">{book.authors}</div>
+              {//!book.authors[0] && (book.authors.map((auth) => (<p>{auth}</p>)))
+              }
+            </div>
+          </li>
+        ))}
+        </ol>
+      </div>
+    </div>);
+};
+};
+
+export default Search;
