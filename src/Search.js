c38c2b54af36c29d6ada1a874da63fbe157caf48
Ishan Lal <ishanlal@gmail.com>
2021-03-08 10:27:42 -0800

    WIP Search.js

--- src/Search.js
+++ src/Search.js
@@ -17,6 +17,12 @@ updateQuery = (query) => {
   }
   else{
     this.setState(() => ({ searched_books: results }));
+    BooksAPI.getAll()
+    .then((books)=> {
+      this.setState(() => ({
+        get_all_books: books
+      }))
+    })
   }
   });
 };
@@ -27,7 +33,7 @@ clearQuery = () => {
 
 handleBookSelect = (book, val) => {
     BooksAPI.update(book, val);
-    console.log('updated');
+    console.log('option changed');
     BooksAPI.getAll()
     .then((books)=> {
       this.setState(() => ({
@@ -64,18 +70,19 @@ render() {
             <li key={book.id} className='book-list-item'>
             <div className="book">
               <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : ''})`
-              }}>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : ''})`}}>
               </div>
-                <div className="book-shelf-changer">
-                  <select value={book.shelf ? book.shelf : "none"} onChange={(e) => this.handleBookSelect(book, e.target.value)}>
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
+              <div className="book-shelf-changer">
+              {//this.state.get_all_books.filter((b) => { return b.id === book.id })
+              }
+                <select value={(this.state.get_all_books.filter((b) => { return b.id === book.id })) ? (this.state.get_all_books.filter((b) => { return b.id === book.id })).shelf : ('none')} onChange={(e) => this.handleBookSelect(book, e.target.value)}>
+                  <option value="move" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
               </div>
               <div className="book-title">{book.title}</div>
               <div className="book-authors">{book.authors && book.authors.map((auth) => <p>{auth}</p>)}</div>
